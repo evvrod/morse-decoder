09a63e1758160bccd3347ac0fc4f2b8aecd9c6a0
Ev-a <evvrod@gmail.com>
2023-08-04 01:24:51 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,45 +1,64 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+    return expr.match(/(.{10}|.)/g).reduce((bacc, bel) => {
+        return bacc + (bel === '**********' ? ' ' : MORSE_TABLE[bel.match(/(.{2}|.)/g).reduce((acc, el) => {
+            return acc + (el === '00' ? '' : (el === '11' ? '-' : '.'));
+        }, '')]);
+    }, '')
 }
+
+// КОДИРОВАНИЕ
+// const MORSE_TABLE_INVERSE = Object.fromEntries(Object.entries(MORSE_TABLE).map(([key, value]) => [value, key]));
+
+// let bin_morse_code = expr.split(' ').reduce((acc, el) => {
+//     let bin_word = el.split('').reduce((wacc, wel) => {
+//         console.log(MORSE_TABLE_INVERSE[wel]);
+//         let bin_morse_code_word = MORSE_TABLE_INVERSE[wel].split('').reduce((bacc, bel) => {
+//             return bacc + (bel == '-' ? '11' : '00')
+//         }, '').padStart(10, '0');
+//         return wacc + bin_morse_code_word;
+//     }, '');
+//     return acc + bin_word + '*'.repeat(10);
+// }, '');
+// return bin_morse_code.slice(0, -10);
 
 module.exports = {
     decode
